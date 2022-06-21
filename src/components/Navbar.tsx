5b73bbe3ff7e436fa308280cea40fb65a964ec68
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-06-21 10:03:40 +0300

    Redesign Navbar for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div>
-      <Link to='/' className='flex items-center gap-5 px-5 py-2 bg-teal-900'>
+      <Link
+        to='/'
+        className='flex items-center justify-center gap-5 px-5 py-2 bg-teal-900 md:justify-start'
+      >
         <img
           src={require('../assets/images/group.png')}
           alt=''
-          className='w-20'
+          className='w-16'
         ></img>
         <div className='text-5xl text-white font-bold font-handwr'>
           Grouplio
