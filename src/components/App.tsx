a6a19f0961966b330d3be01ec7b7d09ed5390abd
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-06-21 10:01:42 +0300

    Create routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,17 @@
+import Layout from './Layout';
+import Home from '../pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
