4730f88d8667434be5b5e7f0c5bed594bbe02938
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-06-22 11:14:25 +0300

    Add Card tests

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,80 @@
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import { PersonDetails } from '../../interfaces/PersonDetails';
+import Card from './Card';
+
+const testData: PersonDetails = {
+  id: 1,
+  name: 'Leanne Graham',
+  username: 'Bret',
+  email: 'Sincere@april.biz',
+  address: {
+    street: 'Kulas Light',
+    suite: 'Apt. 556',
+    city: 'Gwenborough',
+    zipcode: '92998-3874',
+    geo: {
+      lat: '-37.3159',
+      lng: '81.1496',
+    },
+  },
+  phone: '1-770-736-8031 x56442',
+  website: 'hildegard.org',
+  company: {
+    name: 'Romaguera-Crona',
+    catchPhrase: 'Multi-layered client-server neural-net',
+    bs: 'harness real-time e-markets',
+  },
+};
+
+it('renders without crashing', () => {
+  render(
+    <MemoryRouter>
+      <Card {...testData} />
+    </MemoryRouter>
+  );
+});
+
+it('renders correct data', () => {
+  render(
+    <MemoryRouter>
+      <Card {...testData} />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText('Leanne Graham')).toBeInTheDocument();
+});
+
+it('has correct background color on hover', () => {
+  render(
+    <MemoryRouter>
+      <Card {...testData} />
+    </MemoryRouter>
+  );
+
+  userEvent.hover(screen.getByTestId('card'));
+
+  waitFor(() => {
+    expect(screen.getByTestId('card')).toHaveStyle(
+      'background-color: white; box-shadow: 0 0 #0000'
+    );
+  });
+});
+
+it('has correct link color on hover', () => {
+  render(
+    <MemoryRouter>
+      <Card {...testData} />
+    </MemoryRouter>
+  );
+
+  userEvent.hover(screen.getByText('hildegard.org'));
+
+  waitFor(() => {
+    expect(screen.getByText('hildegard.org')).toHaveStyle(
+      'color: #707070; border-bottom-color: #F5F5F5'
+    );
+  });
+});
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,12 @@
+import { PersonDetails } from '../../interfaces/PersonDetails';
 import DetailsBtn from './DetailsBtn';
 
-function Card(person) {
+function Card(person: PersonDetails) {
   return (
-    <div className='card flex flex-col items-center justify-evenly gap-8 border-2 border-solid border-teal-900 rounded-lg w-72 h-80 shadow-xl py-2 md:transition-all md:hover:bg-white md:hover:shadow-none'>
+    <div
+      data-testid='card'
+      className='card flex flex-col items-center justify-evenly gap-8 border-2 border-solid border-teal-900 rounded-lg w-72 h-80 shadow-xl py-2 md:transition-all md:hover:bg-white md:hover:shadow-none'
+    >
       <div className='flex flex-col items-center gap-2'>
         <div className='flex items-center justify-center bg-teal-900 rounded-full w-20 h-20 text-white text-4xl mb-4'>
           {person.name[0]}
