9c80f55c4caea984244a1aed8c13bf57e508e921
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-06-22 14:33:55 +0300

    Remove unnecessary imports

--- src/components/Layout/Navbar.test.tsx
+++ src/components/Layout/Navbar.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter, Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
